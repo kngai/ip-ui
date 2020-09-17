b9b7981e2ed017247c797de5ca2214f99deca257
Kevin Ngai <kevin.ngai@canada.ca>
2020-09-17 15:21:10 -0700

    Remove geolocation

--- src/components/MapIpOl.vue
+++ src/components/MapIpOl.vue
@@ -3,17 +3,6 @@
     <vl-map class="ipMap" :load-tiles-while-animating="true" :load-tiles-while-interacting="true" data-projection="EPSG:4326">
       <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
 
-      <vl-geoloc @update:position="geolocPosition = $event">
-        <template slot-scope="geoloc">
-          <vl-feature v-if="geoloc.position" id="position-feature">
-            <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
-            <vl-style>
-              <vl-style-icon src="_media/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
-            </vl-style>
-          </vl-feature>
-        </template>
-      </vl-geoloc>
-
       <vl-layer-tile id="osm">
         <vl-source-osm></vl-source-osm>
       </vl-layer-tile>
