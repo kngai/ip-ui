8b08fbb6604fef0e63058dd2635591ae1f620afe
Kevin Ngai <kevin.ngai@canada.ca>
2020-09-23 17:03:43 -0700

    Rename project. Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>Not much information here, other than Chewie is a good boy...</p>
+    <p>Not much information here.<br>
+      But here's Chewie.<br>
+      He's a good boy.
+    </p>
     <v-tooltip bottom>
       <template v-slot:activator="{ on, attrs }">
         <v-img src="@/assets/chewie-is-a-good-boy.jpg" width="372" height="496" v-bind="attrs" v-on="on"></v-img>
